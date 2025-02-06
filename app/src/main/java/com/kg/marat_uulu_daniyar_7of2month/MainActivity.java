7bea205d7bf5b1319f46c92660c8048e295679e0
shugganait <daniarm209@gmail.com>
2025-02-07 01:10:32 +0600

    чистка

--- app/src/main/java/com/kg/marat_uulu_daniyar_7of2month/MainActivity.java
+++ app/src/main/java/com/kg/marat_uulu_daniyar_7of2month/MainActivity.java
@@ -1,21 +1,16 @@
 package com.kg.marat_uulu_daniyar_7of2month;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.Objects;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnSecondActivity;
     private TextView textView;
-    private Integer first, second, result;
+    private Integer first=0, second=0, result=0;
     private Boolean isOperationClicked;
     private String chosenOperation;
 
@@ -24,18 +19,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnSecondActivity = findViewById(R.id.btn_to_second_activity);
         textView = findViewById(R.id.text_view);
-        findViewById(R.id.btn_to_second_activity).setOnClickListener(view -> {
-            Intent secondActivity = new Intent(MainActivity.this, SecondActivity.class);
-            secondActivity.putExtra("result", textView.getText().toString());
-            startActivity(secondActivity);
-        });
     }
 
     @SuppressLint("NonConstantResourceId")
     public void onNumberClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btn_one:
                 setOrAppend("1");
                 break;
@@ -72,33 +61,28 @@ public class MainActivity extends AppCompatActivity {
                 second = 0;
                 break;
         }
-        btnSecondActivity.setVisibility(View.GONE);
         isOperationClicked = false;
     }
 
 
     @SuppressLint({"NonConstantResourceId", "SetTextI18n"})
     public void onOperationClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btn_plus:
                 first = Integer.valueOf(textView.getText().toString());
                 chosenOperation = "+";
-                btnSecondActivity.setVisibility(View.GONE);
                 break;
             case R.id.btn_minus:
                 first = Integer.valueOf(textView.getText().toString());
                 chosenOperation = "-";
-                btnSecondActivity.setVisibility(View.GONE);
                 break;
             case R.id.btn_multiplication:
                 first = Integer.valueOf(textView.getText().toString());
                 chosenOperation = "*";
-                btnSecondActivity.setVisibility(View.GONE);
                 break;
             case R.id.btn_division:
                 first = Integer.valueOf(textView.getText().toString());
                 chosenOperation = "/";
-                btnSecondActivity.setVisibility(View.GONE);
                 break;
             case R.id.btn_equal:
                 second = Integer.valueOf(textView.getText().toString());
@@ -106,11 +90,11 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(result.toString());
                 first = 0;
                 second = 0;
-                btnSecondActivity.setVisibility(View.VISIBLE);
                 break;
         }
         isOperationClicked = true;
     }
+
     public void setOrAppend(String num) {
         if (textView.getText().toString().equals("0") || isOperationClicked) {
             textView.setText(num);
@@ -118,8 +102,9 @@ public class MainActivity extends AppCompatActivity {
             textView.append(num);
         }
     }
-    public void chosenOper(String oper){
-        switch (oper){
+
+    public void chosenOper(String oper) {
+        switch (oper) {
             case "+":
                 result = first + second;
                 break;
